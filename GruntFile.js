ee212a75b66adc3ccc98e15f70d4a8ecadfb4055
emerlock <emerlock@live.com>
2014-07-17 18:36:34 -0400

    finishing up setup of grunt w/ files we are currently using. Make sure to change configuration once project is more mature.

--- GruntFile.js
+++ GruntFile.js
@@ -2,7 +2,7 @@
  * Created by emerlock on 6/26/2014.
  */
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     // Project configuration.
     grunt.initConfig({
@@ -12,34 +12,40 @@ module.exports = function(grunt) {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
             build: {
-                src: 'www/resources/js/login.js',
+                src: ['www/resources/js/login.js'],
                 dest: '<%= pkg.name %>.min.js'
             }
         },
-        concat:{
-            build:{
-                src: 'tyejdryj',
-                dest: 'sdgsjsdhj'
+        concat: {
+            build: {
+                src: '',
+                dest: ''
             }
         },
         jshint: {
             // define the files to lint WAT
-            files: ['gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+            files: ['!gruntfile.js', 'express/node_modules/express/lib/authUtils.js',
+                'express/node_modules/express/lib/authentication.js',
+                'frontEnd/www/*.js',
+                '!frontEnd/www/js/external/*.js',
+                '!frontEnd/www/index.js'],
             // configure JSHint (documented at http://www.jshint.com/docs/)
             options: {
-                    jshintrc: '.jshintrc',
-                    reporter: require('jshint-stylish')
+                jshintrc: '.jshintrc',
+                reporter: require('jshint-stylish')
             }
         }
     });
 
-    // Load the plugin that provides the "uglify" task.
+    // Load plugins
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+
+    // current debug, add bundle later
+    grunt.registerTask('debug', ['jshint']);
 
 };
